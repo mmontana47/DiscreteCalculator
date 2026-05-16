43fe9bbb42be588dcae7165723ad9869fa17df6b
mpszargan <m.p.szargan@gmail.com>
2026-05-16 12:30:44 +0200

    Add files via upload

---
+++ src/main/java/pl/edu/uj/discretecalculator/algorithm/DFS.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class DFS<V,E> implements AlgorithmicInterface<V,E>{
+    private final Deque<Vertex<V>> order=new ArrayDeque<>();
+    @Override
+    public String algorithmName()
+    {
+        return "Depth-First Search (DFS)";
+    }
+    @Override
+    public void start(Graph<V,E> graph,Vertex<V> first)
+    {
+        Set<Vertex<V>> visited=new HashSet<>();
+        dfs(graph,first,visited,order);
+    }
+
+    private void dfs(Graph<V,E> graph,Vertex<V> node,Set<Vertex<V>> visited,Deque<Vertex<V>> order)
+    {
+        visited.add(node);
+        order.addLast(node);
+        for(Vertex<V> neighbor:graph.getNeighbors(node))
+        {
+            if(!visited.contains(neighbor))
+            {
+                dfs(graph,neighbor,visited,order);
+            }
+        }
+    }
+}
