f18a72b0b39ed427b5fb27b3355e82b7ed3ba789
Michał Szargan <m.p.szargan@gmail.com>
2026-05-16 14:07:25 +0200

    fixed problems with missing packages

--- src/main/java/pl/edu/uj/discretecalculator/algorithm/AlgorithmicInterface.java
+++ src/main/java/pl/edu/uj/discretecalculator/algorithm/AlgorithmicInterface.java
@@ -1,5 +1,8 @@
 //interface
-
+package pl.edu.uj.discretecalculator.algorithm;
+import pl.edu.uj.discretecalculator.model.graph.Graph;
+import pl.edu.uj.discretecalculator.model.graph.Vertex;
+import pl.edu.uj.discretecalculator.model.graph.Edge;
 public interface AlgorithmicInterface<V,E> {
     //returns name of the algorithm
     String algorithmName();
--- src/main/java/pl/edu/uj/discretecalculator/algorithm/Pair.java
+++ src/main/java/pl/edu/uj/discretecalculator/algorithm/Pair.java
@@ -1,3 +1,7 @@
+package pl.edu.uj.discretecalculator.algorithm;
+import pl.edu.uj.discretecalculator.model.graph.Graph;
+import pl.edu.uj.discretecalculator.model.graph.Vertex;
+import pl.edu.uj.discretecalculator.model.graph.Edge;
 public class Pair<V>{
     private Vertex<V> first;
     private Vertex<V> second;
