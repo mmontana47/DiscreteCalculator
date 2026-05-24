ac14153b2434a3506343b007526a713c803c4227
mmontana47 <michal.malik@student.uj.edu.pl>
2026-05-24 04:19:22 +0200

    Minor changes

--- src/main/java/pl/edu/uj/discretecalculator/model/graph/Vertex.java
+++ src/main/java/pl/edu/uj/discretecalculator/model/graph/Vertex.java
@@ -1,6 +1,7 @@
 package pl.edu.uj.discretecalculator.model.graph;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class Vertex<V> {
     private  final int id;
@@ -32,21 +33,28 @@ public class Vertex<V> {
     }
 
     public int getId(){return id;}
-    public V getValue(){return value;}
+    public V getValue(){ return value; }
     public int getColour(){return colour;}
     public void setValue(V v){value=v;}
     public void setColour(int c){colour=c;}
+
     @Override
-    public  boolean equals(Object o)
-    {
-        if(this==o)return true;
-        if(o==null||getClass()!=o.getClass())return false;
+    public boolean equals(Object o){
+        if(this==o) return true;
+        if(o==null||getClass()!=o.getClass()) return false;
         Vertex<?> vertex=(Vertex<?>)o;
-        return id == vertex.id && colour == vertex.colour && Objects.equals(value, vertex.value);
+        return id == vertex.id;
     }
+
     @Override
     public int hashCode()
     {
-        return  Objects.hash(id,value,colour);
+        return Objects.hash(id);
+    }
+
+    //opcjonalnie do testów
+    @Override
+    public String toString() {
+        return "Vertex{" + "id=" + id + ", colour=" + colour + '}';
     }
 }
