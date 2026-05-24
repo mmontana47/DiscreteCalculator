4d51fb4d08ba9f7707eb837a10bfe889eabe5f3b
mmontana47 <michal.malik@student.uj.edu.pl>
2026-05-24 03:57:24 +0200

    Addded a generic for a universal implementation of the "start" function

--- src/main/java/pl/edu/uj/discretecalculator/algorithm/AlgorithmicInterface.java
+++ src/main/java/pl/edu/uj/discretecalculator/algorithm/AlgorithmicInterface.java
@@ -1,11 +1,10 @@
 //interface
 package pl.edu.uj.discretecalculator.algorithm;
 import pl.edu.uj.discretecalculator.model.graph.Graph;
-import pl.edu.uj.discretecalculator.model.graph.Vertex;
-import pl.edu.uj.discretecalculator.model.graph.Edge;
-public interface AlgorithmicInterface<V,E> {
+
+public interface AlgorithmicInterface<V,R> {
     //returns name of the algorithm
     String algorithmName();
     //starts the algorithm
-    void start(Graph<V,E> graph,Vertex<V> start);
+    R start(Graph<V> graph);
 }
