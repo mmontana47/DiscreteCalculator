91adee416a1692e796711c06fac2871193479892
mmontana47 <michal.malik@student.uj.edu.pl>
2026-05-24 04:11:21 +0200

    New enums for BFS and DFS

--- src/main/java/pl/edu/uj/discretecalculator/controller/Mode.java
+++ src/main/java/pl/edu/uj/discretecalculator/controller/Mode.java
@@ -4,8 +4,9 @@ public enum Mode {
     ADD_VERTEX("Add Vertex", "Click to add a vertex"),
     ADD_EDGE("Add Edge", "Click two vertices to add an edge"),
     DELETE("Delete", "Click a vertex or an edge to delete it"),
-    MOVE("Move", "Click and drag a vertex to change its position");
-
+    MOVE("Move", "Click and drag a vertex to change its position"),
+    RUN_BFS("Run BFS", "Click a vertex to start Breadth-First Search"),
+    RUN_DFS("Run DFS", "Click a vertex to start Depth-First Search");
     private final String label;
     private final String hint;
 
