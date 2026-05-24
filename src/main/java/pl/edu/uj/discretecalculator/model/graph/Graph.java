8532fc1774094b0aec0a31ff4978be1f332c402d
mmontana47 <michal.malik@student.uj.edu.pl>
2026-05-24 04:10:46 +0200

    Changed the model to adjacency edge lists and made necessary changes following from it

--- src/main/java/pl/edu/uj/discretecalculator/model/graph/Graph.java
+++ src/main/java/pl/edu/uj/discretecalculator/model/graph/Graph.java
@@ -1,153 +1,112 @@
 package pl.edu.uj.discretecalculator.model.graph;
+
 import java.util.*;
-import pl.edu.uj.discretecalculator.exception.*;
-public class Graph<V,E> {
-    private final Map<Vertex<V>,List<Vertex<V>>> neighborhoodList=new LinkedHashMap<>();
-    private final List<Vertex<V>> vertices=new ArrayList<>();
-    private final E name;
-    private int size=0;
-    private boolean isDirected=false;
-    private boolean colouredVertices=false;
-    private boolean colouredEdges=false;
 
-    public Graph(E name)
-    {
-        this.name=name;
-    }
-    public Graph(E name,boolean isDirected,boolean colouredVertices,boolean colouredEdges)
-    {
-        this.name=name;
-        this.isDirected=isDirected;
-        this.colouredVertices=colouredVertices;
-        this.colouredEdges=colouredEdges;
+public class Graph<V> {
+
+    private final Map<Vertex<V>, List<Edge<V>>> adjacencyList = new LinkedHashMap<>();
+
+    private final String name;
+    private boolean isDirected = false;
+    private boolean colouredVertices = false;
+    private boolean colouredEdges = false;
+
+    public Graph(String name) {
+        this.name = name;
     }
 
-    public boolean addVertex(Vertex<V> vertex)
-    {
-        if(vertex==null)
-            throw new IllegalArgumentException("Vertex cannot be null");
-        if(neighborhoodList.containsKey(vertex))
-            throw new VertexAlreadyExistsException("Vertex "+vertex.getId()+" is already in the graph.");
-        else
-        {
-            List<Vertex<V>> neighbors=new ArrayList<>();
-            neighborhoodList.put(vertex,neighbors);
-            vertices.add(vertex);
-            size++;
-            return true;
-        }
+    public Graph(String name, boolean isDirected, boolean colouredVertices, boolean colouredEdges) {
+        this.name = name;
+        this.isDirected = isDirected;
+        this.colouredVertices = colouredVertices;
+        this.colouredEdges = colouredEdges;
     }
 
-    public void addEdge(Edge<V> edge)
-    {
-        if(edge==null)
-            throw new IllegalArgumentException("Edge cannot be null.");
-        if(edge.getSource().getId()==edge.getTarget().getId())
-            throw new WrongEdgeTypeException("Edge from vertex to itself is not allowed.");
-        if(!neighborhoodList.containsKey(edge.getSource()))
-        {
-            List<Vertex<V>> list=new ArrayList<>();
-            neighborhoodList.put(edge.getSource(),list);
-            vertices.add(edge.getSource());
-            size++;
-        }
-        if(!neighborhoodList.containsKey(edge.getTarget()))
-        {
-            List<Vertex<V>> list=new ArrayList<>();
-            neighborhoodList.put(edge.getTarget(),list);
-            vertices.add(edge.getTarget());
-            size++;
-        }
-        if(neighborhoodList.get(edge.getSource()).contains(edge.getTarget()))
-            throw new EdgeAlreadyExistsException("Edge from "+edge.getSource().getId()+" to "+edge.getTarget()+" already exists.");
-        if(!neighborhoodList.get(edge.getSource()).contains(edge.getTarget()))
-        {
-            neighborhoodList.get(edge.getSource()).add(edge.getTarget());
-        }
-        if(!isDirected)
-        {
-            if(!neighborhoodList.get(edge.getTarget()).contains(edge.getSource()))
-            {
-                neighborhoodList.get(edge.getTarget()).add(edge.getSource());
-            }
-        }
-    }
-
-    public boolean deleteVertex(Vertex<V> vertex)
-    {
-        if(vertices.contains(vertex))
-            vertices.remove(vertex);
-        if(neighborhoodList.containsKey(vertex))
-        {
-            if(isDirected)
-            {
-                neighborhoodList.remove(vertex);
-                for(Vertex<V> v: vertices)
-                {
-                    neighborhoodList.get(v).remove(vertex);
-                }
-                size--;
-            }
-            else
-            {
-                for(Vertex<V> v: neighborhoodList.get(vertex))
-                {
-                    neighborhoodList.get(v).remove(vertex);
-                }
-                neighborhoodList.remove(vertex);
-                size--;
-            }
-            return true;
-        }
-        else
+    public boolean addVertex(Vertex<V> vertex) {
+        if (adjacencyList.containsKey(vertex)) {
             return false;
+        }
+        adjacencyList.put(vertex, new ArrayList<>());
+        return true;
     }
 
-    public boolean deleteEdge(Edge<V> edge)
-    {
-        if(isDirected)
-        {
-            return neighborhoodList.get(edge.getSource()).remove(edge.getTarget());
+    public void addEdge(Edge<V> edge) {
+        addVertex(edge.getSource());
+        addVertex(edge.getTarget());
+
+        adjacencyList.get(edge.getSource()).add(edge);
+
+        if (!isDirected) {
+            adjacencyList.get(edge.getTarget()).add(edge);
         }
-        else {
-            neighborhoodList.get(edge.getSource()).remove(edge.getTarget());
-            return neighborhoodList.get(edge.getTarget()).remove(edge.getSource());
+    }
+
+    public boolean deleteVertex(Vertex<V> vertex) {
+        if (!adjacencyList.containsKey(vertex)) return false;
+
+        adjacencyList.remove(vertex);
+
+        for (List<Edge<V>> edges : adjacencyList.values()) {
+            edges.removeIf(edge -> edge.getTarget().equals(vertex) || edge.getSource().equals(vertex));
         }
+        return true;
+    }
+
+    public boolean deleteEdge(Edge<V> edge) {
+        boolean removed = false;
+        if (adjacencyList.containsKey(edge.getSource())) {
+            removed = adjacencyList.get(edge.getSource()).remove(edge);
+        }
+        if (!isDirected && adjacencyList.containsKey(edge.getTarget())) {
+            removed = adjacencyList.get(edge.getTarget()).remove(edge) || removed;
+        }
+        return removed;
     }
 
     public List<Vertex<V>> getVertices() {
-        return vertices;
+        return new ArrayList<>(adjacencyList.keySet());
     }
 
-    public E getName(){return name;}
+    public List<Edge<V>> getIncidentEdges(Vertex<V> vertex) {
+        return adjacencyList.getOrDefault(vertex, new ArrayList<>());
+    }
 
     public List<Vertex<V>> getNeighbors(Vertex<V> vertex) {
-        return neighborhoodList.getOrDefault(vertex, new ArrayList<>());
+        List<Vertex<V>> neighbors = new ArrayList<>();
+        for (Edge<V> edge : getIncidentEdges(vertex)) {
+            Vertex<V> neighbor = edge.getSource().equals(vertex) ? edge.getTarget() : edge.getSource();
+            neighbors.add(neighbor);
+        }
+        return neighbors;
     }
+
+    public int getSize() {
+        return adjacencyList.size();
+    }
+
     @Override
-    public boolean equals(Object o)
-    {
-        if(o==this)return true;
-        if(o == null || getClass() != o.getClass())return false;
-        Graph<?,?> graph=(Graph<?, ?>) o;
-        if (size != graph.size ||
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Graph<?> graph = (Graph<?>) o;
+        if (this.getSize() != graph.getSize() ||
                 isDirected != graph.isDirected ||
                 colouredVertices != graph.colouredVertices ||
                 colouredEdges != graph.colouredEdges ||
                 !Objects.equals(name, graph.name)) {
             return false;
         }
-        if (!neighborhoodList.keySet().equals(graph.neighborhoodList.keySet())) {
+        if (!adjacencyList.keySet().equals(graph.adjacencyList.keySet())) {
             return false;
         }
-        for (Map.Entry<Vertex<V>, List<Vertex<V>>> entry : neighborhoodList.entrySet()) {
+        for (Map.Entry<Vertex<V>, List<Edge<V>>> entry : adjacencyList.entrySet()) {
             Vertex<V> currentVertex = entry.getKey();
-            List<Vertex<V>> thisNeighbors = entry.getValue();
-            List<?> otherNeighbors = graph.neighborhoodList.get(currentVertex);
-            if (otherNeighbors == null || thisNeighbors.size() != otherNeighbors.size()) {
+            List<Edge<V>> thisEdges = entry.getValue();
+            List<?> otherEdges = graph.adjacencyList.get(currentVertex);
+            if (otherEdges == null || thisEdges.size() != otherEdges.size()) {
                 return false;
             }
-            if (!new HashSet<>(thisNeighbors).equals(new HashSet<>(otherNeighbors))) {
+            if (!new HashSet<>(thisEdges).equals(new HashSet<>(otherEdges))) {
                 return false;
             }
         }
@@ -155,21 +114,17 @@ public class Graph<V,E> {
     }
 
     @Override
-    public int hashCode()
-    {
-        int result = Objects.hash(name, size, isDirected, colouredVertices, colouredEdges);
+    public int hashCode() {
+        int result = Objects.hash(name, this.getSize(), isDirected, colouredVertices, colouredEdges);
         int edgesHash = 0;
-        for (Map.Entry<Vertex<V>, List<Vertex<V>>> entry : neighborhoodList.entrySet()) {
+        for (Map.Entry<Vertex<V>, List<Edge<V>>> entry : adjacencyList.entrySet()) {
             int keyHash = entry.getKey() != null ? entry.getKey().hashCode() : 0;
-            int neighborsHash = 0;
-            for (Vertex<V> neighbor : entry.getValue()) {
-                neighborsHash += (neighbor != null ? neighbor.hashCode() : 0);
+            int localEdgesHash = 0;
+            for (Edge<V> edge : entry.getValue()) {
+                localEdgesHash += (edge != null ? edge.hashCode() : 0);
             }
-            edgesHash += (keyHash ^ neighborsHash);
+            edgesHash += (keyHash ^ localEdgesHash);
         }
-
         return 31 * result + edgesHash;
     }
 }
-
-
