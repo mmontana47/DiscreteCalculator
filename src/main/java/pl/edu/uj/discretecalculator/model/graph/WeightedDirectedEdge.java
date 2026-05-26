0fb037e8fc4bd39a1b663ebf69eead0c3010da9b
mmontana47 <michal.malik@student.uj.edu.pl>
2026-05-26 21:13:12 +0200

    Added WeightedDirectedEdge.java

---
+++ src/main/java/pl/edu/uj/discretecalculator/model/graph/WeightedDirectedEdge.java
@@ -0,0 +1,50 @@
+package pl.edu.uj.discretecalculator.model.graph;
+
+import java.util.Objects;
+
+public class WeightedDirectedEdge<V> extends Edge<V> {
+    private double weight = 1.0;
+
+    public WeightedDirectedEdge(Vertex<V> source, Vertex<V> target, int id) {
+        super(source, target, id);
+    }
+
+    public WeightedDirectedEdge(Vertex<V> source, Vertex<V> target, int id, double weight) {
+        super(source, target, id);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WeightedDirectedEdge<?> edge = (WeightedDirectedEdge<?>) o;
+        return this.getId() == edge.getId()
+                && Double.compare(edge.weight, weight) == 0
+                && this.getSource().equals(edge.getSource())
+                && this.getTarget().equals(edge.getTarget());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), weight, this.getSource(), this.getTarget());
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "id=" + this.getId() +
+                ", weight=" + weight +
+                ", source=" + this.getSource().getId() +
+                " ----> target=" + this.getTarget().getId() +
+                '}';
+    }
+}
