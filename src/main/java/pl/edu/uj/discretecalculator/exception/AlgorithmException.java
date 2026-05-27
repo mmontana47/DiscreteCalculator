0febef21cf47141cb7610df3b39253d019ed0ca6
Michał Szargan <m.p.szargan@gmail.com>
2026-05-27 16:37:10 +0200

    fixed SCC, created toposort algorithm , added algorithm exceptions

---
+++ src/main/java/pl/edu/uj/discretecalculator/exception/AlgorithmException.java
@@ -0,0 +1,8 @@
+package pl.edu.uj.discretecalculator.exception;
+
+public class AlgorithmException extends RuntimeException{
+    public AlgorithmException(String s)
+    {
+        super(s);
+    }
+}
