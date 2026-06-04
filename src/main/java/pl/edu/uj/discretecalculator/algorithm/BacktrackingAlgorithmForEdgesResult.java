0492e5768f8acb91bfaa93a79e7b7a66ca80cd1a
Michał Szargan <m.p.szargan@gmail.com>
2026-06-04 22:51:11 +0200

    Added Backtracking algorithm for edges

---
+++ src/main/java/pl/edu/uj/discretecalculator/algorithm/BacktrackingAlgorithmForEdgesResult.java
@@ -0,0 +1,18 @@
+package pl.edu.uj.discretecalculator.algorithm;
+import pl.edu.uj.discretecalculator.model.graph.ColouredEdge;
+
+import java.util.*;
+
+public class BacktrackingAlgorithmForEdgesResult<V> {
+    private final ArrayList<ColouredEdge<V>> coloringOrder;
+    private final ArrayList<ColouredEdge<V>> finalColoring;
+
+    public BacktrackingAlgorithmForEdgesResult()
+    {
+        coloringOrder=new ArrayList<>();
+        finalColoring=new ArrayList<>();
+    }
+
+    public ArrayList<ColouredEdge<V>> getColoringOrder(){return coloringOrder;}
+    public ArrayList<ColouredEdge<V>> getFinalColoring(){return finalColoring;}
+}
